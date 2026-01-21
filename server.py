58c9039d78f621fc31643577b1fb5beaaf45db87
Rutuja2139 <wadgaonkarrutuja1@gmail.com>
2026-01-21 21:36:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,4 @@
-#import socket
-#create a socket object
-#bind it to host and port 
-#listen for incoming connections
-#accept a connection and recieve message
-#send response back to client
-#close connection
+
 import socket
 server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.bind(("localhost",9999))
